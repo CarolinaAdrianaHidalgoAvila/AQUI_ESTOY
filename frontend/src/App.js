bb5649885952ef9fc8519cd89132435dd9224fa4
Diego Quiñones <diegoqr2000@gmail.com>
2022-09-09 11:18:55 -0400

    Form para registar nuevo usuario añadido

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import UserProfile from "./pages/UserProfile";
+import NewUserForm from "./components/NewUserForm";
 
 function App() {
   return (
     <>
       <UserProfile />
+      <NewUserForm />
     </>
   );
 }
